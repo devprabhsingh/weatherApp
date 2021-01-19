cc5c2e0e520d6e8bcf98fd26e51d00078a96dbb9
Prabhjot Singh <iprabhsingh0@gmail.com>
2021-01-19 18:32:05 -0500

    Updates

---
+++ precache-manifest.d5ad425494df31b547b5d3544498a208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e59711a6c58aedfbf7a8b5fbf1841a7",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7fb26d703375be440fc2",
+    "url": "/weather-app/static/css/main.050f4fb3.chunk.css"
+  },
+  {
+    "revision": "22730ac82dd86a49daa0",
+    "url": "/weather-app/static/js/2.45e42642.chunk.js"
+  },
+  {
+    "revision": "9f3e154b5a21336fa161f9ed4e97157c",
+    "url": "/weather-app/static/js/2.45e42642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb26d703375be440fc2",
+    "url": "/weather-app/static/js/main.f3ecf092.chunk.js"
+  },
+  {
+    "revision": "20ea9a965a29629f3ab4",
+    "url": "/weather-app/static/js/runtime-main.40d7c4b9.js"
+  }
+]);
